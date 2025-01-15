bd55c038d49c9997e06784a8798bc16b6bd6f222
prtissmart <1048652257@qq.com>
2025-01-15 20:19:31 +0800

    1

--- test_main.py
+++ test_main.py
@@ -1,18 +1,21 @@
 import FaultGraph
 if __name__ == "__main__":
     # shpfile file path
-    SHAPEFILE_PATH = "data/fortest/test3/test3.shp"
+    # enter "data/test1/FaultFromEuropa.shp" or
+    # "data/test2/forTestSpecialCondition.shp"
+    SHAPEFILE_PATH = "data/test1/FaultFromEuropa.shp"
     # Namespace for rdf files
     NAMESPACE = "http://example.org/"
     # Path to the output file
     EXPORT_PATH = "graph_output0109.rdf"
-    # Instantiating Objects
+    # 1.Instantiating Objects
     fault = FaultGraph.FaultGraph(SHAPEFILE_PATH, NAMESPACE)
-    # Generating maps
+    # 2.Generating maps
     fault.generate()
-    # Fault generation time ordering
+    # 3.Fault generation time ordering
+    # ATTENTION:Generate maps frist
     fault.sort()
-    # Show Sorting Results
+    # 4.Show Sorting Results after fault.sort()
     fault.show()
-    # export rdf file
+    # 5.export rdf file
     fault.export_rdf(EXPORT_PATH)
